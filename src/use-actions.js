bfb10af301ec3c30e87a36c8c75ffd990ebb519f
Kait Mikitin <kait@getflex.com>
2022-05-11 11:49:37 -0500

    Create useCounter custom hook to optimize counter methods

--- src/use-actions.js
+++ src/use-actions.js
@@ -1,7 +1,10 @@
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 
 export const useActions = (actions) => {
     const dispatch = useDispatch();
-    return bindActionCreators(actions, dispatch);
-}
+    return useMemo(() => bindActionCreators(actions, dispatch), [actions, dispatch]);
+};
+
+// calling useMemo means we won't have to rebuild that object each time, is more efficient
